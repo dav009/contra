fab408b16be03995252a7514a091e9b675a80826
David Przybilla <dav.alejandro@gmail.com>
2015-07-17 14:32:07 +0100

    adding contract extractor

---
+++ contract_extractor.py
@@ -0,0 +1,39 @@
+import requests
+from multiprocessing import Pool, Value
+import codecs
+
+counter = Value('i', 0)
+
+def worker(pair):
+	url = pair[0]
+	output_folder = pair[1]
+	try:
+		result = requests.get(url)
+		if result.status_code == 200:
+			f = codecs.open(output_folder+"/"+(url.replace("/", "_")), 'w', 'utf-8')
+			f.write(result.text)
+			f.close()
+			#print("downloaded.." + url)
+		else:
+			print("error downloading.." + url)
+	except Exception as e:
+		print(e.message)
+		print("error downloading.." + url)
+	global counter
+	counter.value += 1
+	print("done.." + str(counter.value))
+
+
+def main(args):
+	file_with_urls = args[0]
+	output_folder = args[1]
+
+	f = codecs.open(file_with_urls, 'r', 'utf-8')
+	urls = (("https://www.contratos.gov.co" + line.strip(),output_folder) for line in f)
+		
+
+	pool = Pool(100)
+	pool.map(worker , urls)
+
+
+main(["data/all_links", "data/contracts/"])
